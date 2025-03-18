15670ad10daab7d50200969b6e21db08e2472bba
bapudas <aswinikumardasdas2@gmail.com>
2025-03-18 18:09:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Add interactivity for buttons or dynamic content
+const exploreButton = document.querySelector('.hero button');
+exploreButton.addEventListener('click', () => {
+  alert('Explore button clicked!');
+});
